c3d0f3d6a7a237af1b5764daf3eb1a36b6c7c5c2
shameem <shameem@rslabs.in>
2022-03-04 14:54:20 +0530

    initial

---
+++ pages/abi.js
@@ -0,0 +1,21 @@
+
+ export const minABI = [
+    {
+      constant: true,
+      inputs: [
+        {
+          name: "_owner",
+          type: "address",
+        },
+      ],
+      name: "balanceOf",
+      outputs: [
+        {
+          name: "balance",
+          type: "uint256",
+        },
+      ],
+      payable: false,
+      type: "function",
+    },
+  ];
